216e97828a5752169a5a3e7171bf52f83f856dc1
Lindita <linditakalaj@gmail.com>
2023-03-07 17:52:16 -0500

    connected to vector and added camera feed

--- gui.py
+++ gui.py
@@ -1,10 +1,28 @@
+import asyncio
+import threading
+import time
+
+import anki_vector
 import customtkinter as ctk
+import multiprocessing as mp
+import sys
+
+from PIL import ImageTk
 
 
 class Window(ctk.CTk):
-    def __init__(self):
+    def __init__(self, robot):
         super().__init__()
+        self.label = None
+        self.height = None
+        self.width = None
+        self.vector = robot
+        self.loop = asyncio.get_event_loop()
         self.configure_style()
+        self.label = ctk.CTkLabel(self)
+        self.label.pack(fill=ctk.BOTH, expand=True)
+        self.vector.camera.init_camera_feed()
+        self.start_camera()
 
     def configure_style(self):
         self.configure(background='#303030')
@@ -23,3 +41,12 @@ class Window(ctk.CTk):
 
         # Centers window based on users monitor
         self.geometry('%dx%d+%d+%d' % (width, height, x, y))
+
+    # LOADS SO SLOW- LOOK AT IT LATER
+    def start_camera(self):
+        image = self.vector.camera.latest_image
+        raw_img = image.raw_image
+        print(raw_img)
+        photo_image = ctk.CTkImage(light_image=raw_img, size=(320, 240))
+        self.label.configure(image=photo_image)
+        self.after(1, self.start_camera)
--- main.py
+++ main.py
@@ -1,6 +1,12 @@
+import threading
+
+import anki_vector
+from anki_vector import util
+
 import gui
 
 if __name__ == "__main__":
-    gui.Window().mainloop()
-
-
+    args = util.parse_command_args()
+    with anki_vector.AsyncRobot(args.serial) as robot:
+        root = gui.Window(robot)
+        root.mainloop()
