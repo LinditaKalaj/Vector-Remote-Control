47169122796007cbf1a9d333c143d51c127152a7
Lindita <linditakalaj@gmail.com>
2023-03-12 19:01:50 -0400

    added entry input for user to speak to vector

--- gui.py
+++ gui.py
@@ -19,21 +19,24 @@ class Window(ctk.CTk):
         self.vector = robot
         self.loop = asyncio.get_event_loop()
         self.configure_style()
-        self.label = ctk.CTkLabel(self)
+        self.label = ctk.CTkLabel(self, text="")
         self.label.pack(fill=ctk.BOTH, expand=True)
+        self.speak_entry = ctk.CTkEntry(self, placeholder_text="Enter what you want Vector to say here!")
+        self.speak_entry.pack(fill=ctk.BOTH, expand=True)
         self.vector.camera.init_camera_feed()
         self.start_camera()
+        self.speak_entry.bind("<Return>", lambda event: robot.behavior.say_text(self.speak_entry.get()))
 
     def configure_style(self):
         self.configure(background='#303030')
-        self.title("ezVector Setup")
+        self.title("Vector Remote")
         ctk.set_default_color_theme("green")
 
         # Set min and max sizes
-        width = 500
-        height = 700
-        self.minsize(400, 600)
-        self.maxsize(500, 700)
+        height = 500
+        width = 700
+        self.minsize(700, 500)
+        self.maxsize(700, 500)
         screen_width = self.winfo_screenwidth()
         screen_height = self.winfo_screenheight()
         x = (screen_width / 2) - (width / 2)
@@ -44,9 +47,15 @@ class Window(ctk.CTk):
 
     # LOADS SO SLOW- LOOK AT IT LATER
     def start_camera(self):
-        image = self.vector.camera.latest_image
-        raw_img = image.raw_image
-        print(raw_img)
-        photo_image = ctk.CTkImage(light_image=raw_img, size=(320, 240))
+        image = self.vector.camera.latest_image.raw_image
+        print(image)
+        photo_image = ctk.CTkImage(light_image=image, size=(453, 339))
         self.label.configure(image=photo_image)
-        self.after(1, self.start_camera)
+        self.after(20, self.start_camera)
+
+    # Vectors speech
+    def vector_speak(self):
+        to_say = self.speak_entry.get()
+        if to_say == "":
+            return
+        self.vector.behavior.say_text(to_say)
