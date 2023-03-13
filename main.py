d31fd7689fab88d8ffc227330f95191bac2e36ce
Lindita <linditakalaj@gmail.com>
2023-03-13 18:22:50 -0400

    error handling for bad connections

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
-import threading
-
 import anki_vector
 from anki_vector import util
+from anki_vector.exceptions import VectorConnectionException, VectorNotFoundException
 
 import gui
 
 if __name__ == "__main__":
     args = util.parse_command_args()
-    with anki_vector.AsyncRobot(args.serial) as robot:
-        root = gui.Window(robot)
-        root.mainloop()
+    try:
+        with anki_vector.AsyncRobot(args.serial) as robot:
+            root = gui.Window(robot)
+            root.mainloop()
+    except VectorNotFoundException as v:
+        print("A connection error occurred: %s" % v)
+    except VectorConnectionException as e:
+        print("A connection error occurred: %s" % e)
+
+
