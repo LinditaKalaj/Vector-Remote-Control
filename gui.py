9df9537628dc7f2d5c28d240d8e6be60f22eb08e
Lindita <linditakalaj@gmail.com>
2023-03-13 20:42:59 -0400

    added listeners for directional inputs to control vector

--- gui.py
+++ gui.py
@@ -18,6 +18,7 @@ class Window(ctk.CTk):
         self.configure_style()
         self.video = ctk.CTkLabel(self, text="")
         self.video.pack(fill=ctk.BOTH, expand=True)
+        self.video.bind("<Button-1>", lambda event: self.video.focus())
         self.clear_text = ctk.CTkButton(self, text="Clear text", command=lambda: self.speak_entry.delete(0, ctk.END))
         self.speak_entry = ctk.CTkEntry(self, placeholder_text="Enter what you want Vector to say here!")
         self.clear_text.pack(fill=ctk.BOTH, expand=True)
@@ -25,7 +26,14 @@ class Window(ctk.CTk):
         self.vector.camera.init_camera_feed()
         self.start_camera()
         self.speak_entry.bind("<Return>", lambda event: robot.behavior.say_text(self.speak_entry.get()))
-
+        self.bind("<KeyPress-Up>", self.up_arrow)
+        self.bind("<KeyRelease-Up>", self.stop)
+        self.bind("<KeyPress-Down>", self.down_arrow)
+        self.bind("<KeyRelease-Down>", self.stop)
+        self.bind("<KeyPress-Left>", self.left_arrow)
+        self.bind("<KeyRelease-Left>", self.stop)
+        self.bind("<KeyPress-Right>", self.right_arrow)
+        self.bind("<KeyRelease-Right>", self.stop)
     def configure_style(self):
         self.configure(background='#303030')
         self.title("Vector Remote")
@@ -47,7 +55,6 @@ class Window(ctk.CTk):
     # LOADS SO SLOW- LOOK AT IT LATER
     def start_camera(self):
         image = self.vector.camera.latest_image.raw_image
-        print(image)
         photo_image = ctk.CTkImage(light_image=image, size=(453, 339))
         self.video.configure(image=photo_image)
         self.after(20, self.start_camera)
@@ -58,3 +65,22 @@ class Window(ctk.CTk):
         if to_say == "":
             return
         self.vector.behavior.say_text(to_say)
+
+    def up_arrow(self, x):
+        print("Up arrow pressed")
+        self.vector.motors.set_wheel_motors(300, 300)
+
+    def left_arrow(self, x):
+        print("Left arrow pressed")
+        self.vector.motors.set_wheel_motors(-300, 300)
+
+    def right_arrow(self, x):
+        print("Right arrow pressed")
+        self.vector.motors.set_wheel_motors(300, -300)
+
+    def down_arrow(self, x):
+        print("Down arrow pressed")
+        self.vector.motors.set_wheel_motors(-300, -300)
+
+    def stop(self, x):
+        self.vector.motors.set_wheel_motors(0, 0)
