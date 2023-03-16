df8900eed1a8e15ce11b5a123501d1ccb4eeb40d
Lindita <linditakalaj@gmail.com>
2023-03-16 19:11:56 -0400

    added grid layout to gui and button for connection

--- gui.py
+++ gui.py
@@ -2,33 +2,44 @@ import asyncio
 import threading
 import time
 
-import anki_vector
 import customtkinter as ctk
+from tkinter import messagebox
+import anki_vector
+from PIL import Image
+from anki_vector import util
+from anki_vector.exceptions import VectorConnectionException, VectorNotFoundException, VectorConfigurationException
 import multiprocessing as mp
 import sys
 
 from PIL import ImageTk
 
+from landing_gui import LandingWindow
 from move_vector import MoveVector
 
 
 class Window(ctk.CTk):
-    def __init__(self, robot):
+    def __init__(self):
         super().__init__()
+        self.connection_status = None
+        self.connect_button = None
+        self.speak_entry = None
+        self.clear_text = None
         self.video = None
-        self.vector = robot
-        self.move_vector = MoveVector(robot)
+        self.vector = None
+        self.move_vector = None
         self.configure_style()
-        self.video = ctk.CTkLabel(self, text="")
-        self.video.pack(fill=ctk.BOTH, expand=True)
-        self.clear_text = ctk.CTkButton(self, text="Clear text", command=lambda: self.speak_entry.delete(0, ctk.END))
-        self.speak_entry = ctk.CTkEntry(self, placeholder_text="Enter what you want Vector to say here!")
-        self.clear_text.pack(fill=ctk.BOTH, expand=True)
-        self.speak_entry.pack(fill=ctk.BOTH, expand=True)
-        self.vector.camera.init_camera_feed()
-        self.start_camera()
-        self.set_binding()
-        self.move()
+        self.configure_grid()
+        self.configure_items()
+        # self.video = ctk.CTkLabel(self, text="")
+        # self.video.pack(fill=ctk.BOTH, expand=True)
+        # self.clear_text = ctk.CTkButton(self, text="Clear text", command=lambda: self.speak_entry.delete(0, ctk.END))
+        # self.speak_entry = ctk.CTkEntry(self, placeholder_text="Enter what you want Vector to say here!")
+        # self.clear_text.pack(fill=ctk.BOTH, expand=True)
+        # self.speak_entry.pack(fill=ctk.BOTH, expand=True)
+        # self.vector.camera.init_camera_feed()
+        # self.start_camera()
+        # self.set_binding()
+        # self.move()
 
     def configure_style(self):
         self.configure(background='#303030')
@@ -36,10 +47,10 @@ class Window(ctk.CTk):
         ctk.set_default_color_theme("green")
 
         # Set min and max sizes
-        height = 500
-        width = 700
-        self.minsize(700, 500)
-        self.maxsize(700, 500)
+        height = 700
+        width = 900
+        self.minsize(900, 700)
+        self.maxsize(900, 700)
         screen_width = self.winfo_screenwidth()
         screen_height = self.winfo_screenheight()
         x = (screen_width / 2) - (width / 2)
@@ -47,6 +58,45 @@ class Window(ctk.CTk):
 
         # Centers window based on users monitor
         self.geometry('%dx%d+%d+%d' % (width, height, x, y))
+
+    def configure_grid(self):
+        # Configures row and col weights
+        self.grid_rowconfigure(0, weight=1)
+        self.grid_rowconfigure(1, weight=1)
+        self.grid_rowconfigure(2, weight=1)
+        self.grid_rowconfigure(3, weight=1)
+        self.grid_rowconfigure(4, weight=1)
+        self.grid_rowconfigure(5, weight=1)
+        self.grid_rowconfigure(6, weight=1)
+        self.grid_rowconfigure(7, weight=1)
+        self.grid_rowconfigure(8, weight=1)
+        self.grid_rowconfigure(9, weight=1)
+        self.grid_rowconfigure(10, weight=1)
+        self.grid_rowconfigure(11, weight=1)
+        self.grid_rowconfigure(12, weight=1)
+        self.grid_rowconfigure(13, weight=1)
+        self.grid_rowconfigure(14, weight=1)
+        self.grid_rowconfigure(15, weight=1)
+        self.grid_rowconfigure(16, weight=1)
+        self.grid_columnconfigure(0, weight=1)
+        self.grid_columnconfigure(1, weight=1)
+        self.grid_columnconfigure(2, weight=1)
+        self.grid_columnconfigure(3, weight=1)
+        self.grid_columnconfigure(4, weight=1)
+
+    def configure_items(self):
+        self.connect_button = ctk.CTkButton(self, text="Connect", command=lambda: self.connect_to_vector())
+        self.connect_button.grid(row=0, column=4, columnspan=1, padx=10,  sticky="e")
+        self.connection_status = ctk.CTkLabel(self, text="Status: Disconnected")
+        self.connection_status.grid(row=0, column=0, columnspan=1, sticky="nsew")
+        self.video = ctk.CTkLabel(self, text="")
+        photo_image = ctk.CTkImage(light_image=Image.open("./assets/blank_camera.png"), size=(453, 339))
+        self.video.configure(image=photo_image)
+        self.video.grid(row=1, column=0, columnspan=5, rowspan=6, sticky="ew")
+        self.clear_text = ctk.CTkButton(self, text="Clear text", command=lambda: self.speak_entry.delete(0, ctk.END))
+        self.clear_text.grid(row=13, column=4, columnspan=1)
+        self.speak_entry = ctk.CTkEntry(self, placeholder_text="Enter what you want Vector to say here!")
+        self.speak_entry.grid(row=14, column=0, columnspan=5, rowspan=2, sticky="nsew")
 
     def set_binding(self):
         self.video.bind("<Button-1>", lambda event: self.video.focus())
@@ -108,3 +158,25 @@ class Window(ctk.CTk):
     def move(self):
         self.move_vector.main_move()
         self.after(100, self.move)
+
+    def connect_to_vector(self):
+        args = util.parse_command_args()
+        try:
+            self.vector = anki_vector.AsyncRobot(args.serial)
+            self.vector.connect()
+        except VectorNotFoundException as v:
+            print("1A connection error occurred: %s" % v)
+        except VectorConnectionException as e:
+            print("2A connection error occurred: %s" % e)
+            messagebox.showerror('Error!', "Connection error occurred: Please try reconnecting.")
+        except VectorConfigurationException as config_e:
+            messagebox.showerror('Error!', "Could not find the sdk configuration file. Please run ezVector Setup "
+                                           "prior to opening this program.")
+            print("3A connection error occurred: %s" % config_e)
+        finally:
+            if self.vector:
+                self.vector.camera.init_camera_feed()
+                self.start_camera()
+                self.move_vector = MoveVector(self.vector)
+                self.set_binding()
+                self.move()
--- main.py
+++ main.py
@@ -1,18 +1,11 @@
 import anki_vector
 from anki_vector import util
-from anki_vector.exceptions import VectorConnectionException, VectorNotFoundException
+from anki_vector.exceptions import VectorConnectionException, VectorNotFoundException, VectorConfigurationException
 
 import gui
+import landing_gui
 
 if __name__ == "__main__":
-    args = util.parse_command_args()
-    try:
-        with anki_vector.AsyncRobot(args.serial) as robot:
-            root = gui.Window(robot)
-            root.mainloop()
-    except VectorNotFoundException as v:
-        print("A connection error occurred: %s" % v)
-    except VectorConnectionException as e:
-        print("A connection error occurred: %s" % e)
-
+    root = gui.Window()
+    root.mainloop()
 
