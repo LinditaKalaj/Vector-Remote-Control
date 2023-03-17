e92c1ed262d7b8b573c14ad3fe0dad2244583b85
Lindita <linditakalaj@gmail.com>
2023-03-16 20:39:20 -0400

    cleanup

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import anki_vector
-from anki_vector import util
-from anki_vector.exceptions import VectorConnectionException, VectorNotFoundException, VectorConfigurationException
-
 import gui
-import landing_gui
 
 if __name__ == "__main__":
     root = gui.Window()
