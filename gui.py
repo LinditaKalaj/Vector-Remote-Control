6ffb31749a9e6b41bb5549f54ac186238e8c6cf5
Lindita <linditakalaj@gmail.com>
2023-03-27 14:28:02 -0400

    implemented speed and volume. added fix for list animation timeouts

--- gui.py
+++ gui.py
@@ -7,15 +7,24 @@ from tkinter import messagebox
 import anki_vector
 from PIL import Image
 from anki_vector import util
-from anki_vector.exceptions import VectorConnectionException, VectorNotFoundException, VectorConfigurationException
+from anki_vector.exceptions import VectorConnectionException, VectorNotFoundException, VectorConfigurationException, \
+    VectorControlTimeoutException
+from anki_vector.events import Events
 
 from animations import Animations
+from chat_gpt import ChatGPT
 from move_vector import MoveVector
+from speed import Speed
+from volume import Volume
+
+import anki_vector.events
 
 
 class Window(ctk.CTk):
     def __init__(self):
         super().__init__()
+        self.speed = None
+        self.volume = None
         self.start_video = False
         self.animations = None
         self.blank_photo_image = None
@@ -70,31 +79,34 @@ class Window(ctk.CTk):
         self.grid_rowconfigure(3, weight=1)
         self.grid_rowconfigure(4, weight=1)
         self.grid_rowconfigure(5, weight=1)
-        self.grid_rowconfigure(6, weight=1)
-        self.grid_rowconfigure(7, weight=1)
-        self.grid_rowconfigure(8, weight=1)
-        self.grid_rowconfigure(9, weight=1)
         self.grid_columnconfigure(0, weight=1)
         self.grid_columnconfigure(1, weight=1)
         self.grid_columnconfigure(2, weight=1)
+        self.grid_columnconfigure(3, weight=1)
+        self.grid_columnconfigure(4, weight=1)
 
     def configure_items(self):
         self.connect_button = ctk.CTkButton(self, text="Connect", command=lambda: self.initialize_vector_connection())
         self.connect_button.grid(row=0, column=4, columnspan=1, padx=10,  sticky="e")
         self.connection_status = ctk.CTkLabel(self, text="Status: Disconnected")
-        self.connection_status.grid(row=0, column=3, sticky="nsew")
+        self.connection_status.grid(row=0, column=3, sticky="e")
+        self.volume = Volume(self)
+        self.volume.grid(row=1, column=0, columnspan=3, sticky="sew", padx=(30, 0))
+        self.speed = Speed(self)
+        self.speed.grid(row=2, column=0, columnspan=3, sticky="nsew", pady=(10, 0), padx=(30, 0))
+
         self.video = ctk.CTkLabel(self, text="")
         self.blank_photo_image = ctk.CTkImage(light_image=Image.open("./assets/blank_camera.png"), size=(453, 339))
         self.video.configure(image=self.blank_photo_image)
-        self.video.grid(row=1, column=0, columnspan=5, rowspan=5, sticky="ew")
+        self.video.grid(row=1, column=3, columnspan=3, rowspan=3, sticky=" ew")
 
         self.animations = Animations(master=self)
-        self.animations.grid(row=7, column=0, columnspan=4, rowspan=1, padx=10, sticky="ew")
+        self.animations.grid(row=4, column=0, columnspan=4, padx=(20, 0), sticky="sew")
 
         self.clear_text = ctk.CTkButton(self, text="Clear text", command=lambda: self.speak_entry.delete(0, ctk.END))
-        self.clear_text.grid(row=7, column=4, columnspan=1, sticky="se", pady=17, padx=10)
+        self.clear_text.grid(row=4, column=4, columnspan=1, sticky="se", pady=0, padx=(0, 10))
         self.speak_entry = ctk.CTkEntry(self, placeholder_text="Enter what you want Vector to say here!")
-        self.speak_entry.grid(row=8, column=0, columnspan=5, rowspan=1, sticky="nsew", padx=10)
+        self.speak_entry.grid(row=5, column=0, columnspan=5, rowspan=1, sticky="nsew", padx=10, pady=(10, 10))
 
     def set_binding(self):
         self.video.bind("<Button-1>", lambda event: self.video.focus())
@@ -165,8 +177,9 @@ class Window(ctk.CTk):
     async def connect_to_vector(self):
         args = util.parse_command_args()
         try:
-            self.vector = anki_vector.AsyncRobot(args.serial)
+            self.vector = anki_vector.AsyncRobot(args.serial, behavior_activation_timeout=30.0, cache_animation_lists=False)
             self.vector.connect()
+
             anim_trigger_names = self.vector.anim.anim_trigger_list
             for anim_trigger_name in anim_trigger_names:
                 print(anim_trigger_name)
@@ -180,7 +193,12 @@ class Window(ctk.CTk):
             messagebox.showerror('Error!', "Could not find the sdk configuration file. Please run ezVector Setup "
                                            "prior to opening this program.")
             print("3A connection error occurred: %s" % config_e)
-        finally:
+        except VectorControlTimeoutException as e:
+            print("2A connection error occurred: %s" % e)
+            messagebox.showerror('Error!', "Failed to get control of Vector. Please verify that Vector is connected "
+                                           "to the internet, is on a flat surface, and is fully charged.")
+
+        else:
             if self.vector:
                 self.start_video = True
                 self.vector.camera.init_camera_feed()
@@ -190,6 +208,8 @@ class Window(ctk.CTk):
                 self.move()
                 self.connection_status.configure(text="Status: Connected")
                 self.animations.set_vector(self.vector)
+                self.volume.set_vector(self.vector)
+                self.speed.set_move(self.move_vector)
 
     # Runs async thread
     def async_thread(self):
@@ -211,6 +231,8 @@ class Window(ctk.CTk):
             self.connect_button.grid(row=0, column=4, columnspan=1, padx=10, sticky="e")
 
     def disconnect_vector(self):
+        if not self.vector:
+            return
         self.start_video = False
         self.unbind_movement_bindings()
         self.connect_button.configure(state="disabled", command=None)
