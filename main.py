fffef64867bef4a6e671a56860263d46b6d2a26d
Lindita <linditakalaj@gmail.com>
2023-03-28 19:04:11 -0400

    added menubar to window

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
+from tkinter import Menu
+
 import gui
+from menu import MenuBar
 
 if __name__ == "__main__":
     root = gui.Window()
+    menubar = MenuBar(root)
+    menubar.grid(row=0, column=0, columnspan=5, sticky="new")
     root.mainloop()
 
