24f5b5087a82ef80ff133685a584a0b937d8d1c8
Lindita <linditakalaj@gmail.com>
2023-03-28 18:28:46 -0400

    added menu item to ask user for openai api key

--- menu.py
+++ menu.py
@@ -14,7 +14,7 @@ class MenuBar(ctk.CTkFrame):
         file_menu = tk.Menu(file, tearoff=0, borderwidth=0)
         file_menu.config(borderwidth=0, activeborderwidth=0, bd=0)
         file_menu.add_command(label='GPT API Key', background='gray17', foreground='gray81', activeforeground='gray98',
-                              activebackground='#2CC985')
+                              activebackground='#2CC985', command= self.open_gpt_api_dialog)
         file_menu.add_separator(background='gray17')
         file_menu.add_command(label='Quit', background='gray17', foreground='gray81', activeforeground='gray98',
                               activebackground='#2CC985')
@@ -29,3 +29,20 @@ class MenuBar(ctk.CTkFrame):
                               activebackground='#2CC985')
         help_button.config(menu=help_menu)
         help_button.pack(side='left', pady=1)
+
+    def open_gpt_api_dialog(self):
+        width=300
+        height=150
+        screen_width = self.winfo_screenwidth()
+        screen_height = self.winfo_screenheight()
+        x = (screen_width / 2) - (width / 2)
+        y = (screen_height / 2) - (height / 2)
+
+        dialog = ctk.CTkInputDialog(text="Paste in your GPT OpenAI api key here:", title="GPT OpenAI Api")
+        dialog.geometry('%dx%d+%d+%d' % (width, height, x, y))
+        dialog_input = dialog.get_input()
+        if not dialog_input:
+            return
+        with open(".env", "w") as f:
+            new_api = 'OPENAI_API_KEY=' + dialog_input
+            f.write(new_api)
