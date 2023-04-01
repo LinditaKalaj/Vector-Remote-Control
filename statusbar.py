9b44265d07c88502fbdc74fe6034c1d09f1ae1cb
Lindita <linditakalaj@gmail.com>
2023-04-01 14:49:53 -0400

    removed print statements

--- statusbar.py
+++ statusbar.py
@@ -14,21 +14,15 @@ class StatusBar(ctk.CTkFrame):
         self.vector = robot
 
     def tracking_status(self):
-        print(self.vector.status.are_motors_moving)
         if self.vector.status.is_animating:
-            print("Vector is animating.")
             self.status.configure(text="Vector is animating.")
         elif self.vector.status.is_picked_up:
-            print("Vector is picked up.")
             self.status.configure(text="Vector is picked up.")
         elif self.vector.status.are_motors_moving:
-            print("Vector is moving.")
             self.status.configure(text="Vector is moving.")
         elif self.vector.status.is_charging:
-            print("Vector is currently charging.")
             self.status.configure(text="Vector is currently charging.")
         elif self.vector.status.is_in_calm_power_mode:
-            print("Vector is sleeping")
             self.status.configure(text="Vector is sleeping.")
         else:
             self.status.configure(text="Vector is chilling.")
