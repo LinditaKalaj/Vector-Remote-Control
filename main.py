86a6c9b220587a9dc5d8bdc320d97faf68090a5f
Lindita <linditakalaj@gmail.com>
2023-04-18 15:56:49 -0400

    cleanup + comments

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
-from tkinter import Menu
-
 import gui
 from menu import MenuBar
 
 if __name__ == "__main__":
+    # Adds menu bar to Vectors control window and displays window
     root = gui.Window()
     menubar = MenuBar(root)
     menubar.grid(row=0, column=0, columnspan=5, sticky="new")
     root.mainloop()
-
