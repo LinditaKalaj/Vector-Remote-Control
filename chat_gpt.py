3719e15841f009bed67de9c18e165855169569d7
Lindita <linditakalaj@gmail.com>
2023-04-19 15:41:08 -0400

    added documentation and cleanup

--- chat_gpt.py
+++ chat_gpt.py
@@ -1,20 +1,46 @@
 import os
-
 import openai
-import anki_vector
 from dotenv import load_dotenv
 
 
 class ChatGPT:
+    """
+    Class used to connect to gpt and send questions
+
+    Attributes
+    ----------
+
+    self.vector: object
+        Vectors object to utilize his sdk
+
+    self.history: str
+        Chat log saved so gpt knows the whole scope of the convo. Added first prompt to get Vector in character
+
+    Methods
+    -------
+    ask_gpt(self, question)
+        Sends gpt the question or comment the user typed in the entry box
+    """
+
     def __init__(self):
         self.vector = None
+
+        # load api key from .env file
         load_dotenv()
         openai.api_key = os.getenv("OPENAI_API_KEY")
+
         self.history = [{"role": "system", "content": "You a small robotic companion named Vector. You are funny, "
                                                       "sarcastic and have a great sense of humor. Sometimes people "
                                                       "annoy you but you like them anyways."}]
 
     def ask_gpt(self, question):
+        """
+        Appends the question to history, sends it to gpt and receives a response.
+
+        :param question: str, The question being sent to gpt
+        :return: str, gpt response to the question asked
+        """
+
         self.history.append({"role": "user", "content": question})
         ai_response = openai.ChatCompletion.create(
             model="gpt-3.5-turbo",
